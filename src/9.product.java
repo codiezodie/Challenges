0577bd706ddbd1871368bf7fe5bf4821f7257a78
Zainab Ahmad <zainab14ahmad@gmail.com>
2025-12-07 18:18:28 +0530

    Fix commit message for product.java
    Corrected the previous commit message for product.java

--- src/9.product.java
+++ src/9.product.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+// Correcting previous commit message for product.java
 class product {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
